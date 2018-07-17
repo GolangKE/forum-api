39d7a426b912e900587064bf77808a196e46643c
Samuel Ralak <samuelralak@hotmail.com>
2018-07-17 17:51:01 +0300

    Problem: No initial config and db config
    Solution: Added db config to config file. Initialising Config before startup. Created scipts to create/drop and manage database migrations. Installed the migrate package.

--- config/config.go
+++ config/config.go
@@ -1,1 +1,52 @@
 package config
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	// use gorm postgres dialect
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var repo *gorm.DB
+
+// Init config
+func Init() {
+	// start db connection
+	initDB()
+}
+
+// Repo returns database
+func Repo() *gorm.DB {
+	return repo
+}
+
+// unexported functions
+
+// InitDB initialises database connection
+func initDB() {
+	db, err := gorm.Open(os.Getenv("DB_DIALECT"), dbConfig())
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	repo = db
+	defer db.Close()
+}
+
+func dbConfig() string {
+	str := "host=" + os.Getenv("DB_HOST") +
+		" port=" + os.Getenv("DB_PORT") +
+		" user=" + os.Getenv("DB_USERNAME") +
+		" dbname=" + os.Getenv("DATABASE") +
+		" password=" + os.Getenv("DB_PASSWORD")
+
+	if gin.IsDebugging() {
+		// disable ssl mode if we are in development
+		str = str + " sslmode=disable"
+	}
+
+	return str
+}
--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"./config"
 	"./web"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// initialise app config
+	config.Init()
+
 	router := gin.Default()
 
+	// register app router
 	web.Register(router)
 
 	router.Run()
