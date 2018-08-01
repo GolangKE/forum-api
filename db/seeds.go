43255301d91a9a46db70e3031d3cd8776f4b18ff
Samuel Ralak <samuelralak@hotmail.com>
2018-08-01 15:52:15 +0300

    Problem: No categories have been added
    Solution: Created categories migration files and model and added categories seed data

--- db/seeds.go
+++ db/seeds.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"strings"
+
 	"../web/models"
 	"github.com/jinzhu/gorm"
 )
@@ -11,6 +13,9 @@ func Seed(db *gorm.DB) error {
 
 	// add users to database
 	createUsers(tx)
+
+	// add categories to database
+	createCategories(tx)
 
 	return tx.Commit().Error
 }
@@ -29,6 +34,42 @@ func createUsers(db *gorm.DB) error {
 		if err := db.Create(&user).Error; err != nil {
 			db.Rollback()
 			return err
+		}
+	}
+
+	return nil
+}
+
+func createCategories(db *gorm.DB) error {
+	categories := []string{
+		"Help/Questions",
+		"Code Review",
+		"Releases",
+		"Discussions",
+		"Golang News",
+		"Confs & Meet Ups",
+		"Show & Tell",
+		"Announcements",
+		"Sponsor Messages",
+		"Learning Resources",
+		"Site Feedback",
+		"Jobs",
+	}
+
+	for _, c := range categories {
+		code := strings.ToUpper(c)
+		category := models.Category{
+			Name:     c,
+			Code:     strings.Replace(code, " ", "-", -1),
+			IsActive: true,
+		}
+
+		if db.Where("code = ?", category.Code).First(&category).RecordNotFound() {
+			// create category. rollback if an error occurs
+			if err := db.Create(&category).Error; err != nil {
+				db.Rollback()
+				return err
+			}
 		}
 	}
 
